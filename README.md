31ccd048bfcef9d9452b847fa2bb97284c0adeae
cag000 <emberspirit404@gmail.com>
2019-09-19 20:59:09 +0700

    make beauty

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # scraper
 how it works, don't ask
 
+```
 Need :
 python 3
 scrapy
-
+```
 scrapy crawl -a filename=url.txt -o name.csv
--- scname/scname/spiders/walang.py
+++ scname/scname/spiders/walang.py
@@ -1,23 +1,23 @@
 import scrapy
-
+from scrapy import Request
 
 
 class QuotesSpider(scrapy.Spider):
     name = "scname"
-
+    handle_httpstatus_list = [301,302]
+    handle_httpstatus_all = True
     def __init__(self, filename=None):
         if filename:
             with open(filename) as f:
                 content = f.readlines()
                 self.start_urls = ['https://twitter.com/'+x.strip() for x in content] #fuck this looping all listaaa
-                    
 
     def parse(self, response):
+    
         for title in response.css('title'):
             for quote in response.css('body.ms-windows'):
                 yield {
-
                     'Title': title.css('title::text').extract_first(),
                     'Akun': quote.css('.ProfileWarningTimeline-heading::text').extract_first()
-                    
                 }
+ 
